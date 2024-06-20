5050e3846b92aa0a63b4431ad7ff2ac87b82ae9b
Nicholas <kerbal2346@gmail.com>
2024-06-19 22:54:02 -0600

    Basic semi working ver

--- Tools/tabify/maketabs.c
+++ Tools/tabify/maketabs.c
@@ -2,8 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAXARRAY 1000000
+#define MAXLEN 1000000
+#define MAXLINE 9999
 #define MAXFILENAME 9999
+
+void addTabs(FILE *src, FILE *dst, int spacePerTab){
+	int i = 0;
+	int dstChar = 0;
+	int count = 0;
+	char *inpDat;
+	char *outDat;
+
+	while(fgets(inpDat, MAXLEN - 1, src)) {
+		while (inpDat[i] == ' ') {
+			count++;
+			if (count == spacePerTab) {
+				outDat[dstChar] = '\t';
+				dstChar++;
+			}
+			i++;
+		}
+	}
+
+	strncat(outDat, inpDat, MAXLINE);
+	fputs(outDat, dst);
+}
 
 int main(int argc, char *argv[]){
 	
@@ -11,11 +34,7 @@ int main(int argc, char *argv[]){
 	FILE *outFile;
 	char inpFilename[MAXFILENAME];
 	char outFilename[MAXFILENAME];
-	int spacePerTab = 4; // Default value for my editor
-
-	outFilename[0] = '\0';
-
-	char c;	
+	int spacePerTab = 4; // Default value for my editor	
 
 	printf("Tabify, written by Nicholas West\n");
 
@@ -25,38 +44,63 @@ int main(int argc, char *argv[]){
 		return 0;
 	}
 	
+	inpFilename[0] = '\0';
+
 	// Get input filename first off
 	strncat(inpFilename, *(1 + argv), MAXFILENAME - 1);
+	strncpy(outFilename, inpFilename, MAXFILENAME - 1);
+
+	char c;	
 
 	// Loop for every argv and match options
-	while (--argc > 0 && (*++argv)[0] == '-') {
-		while (c = *++argv[0]) {
-			switch (c) {
-			case 'h': case 'H': // Help case
-				printf("Tabify: Turns a file with spaces as indentation into tabs.\n");
-				printf("Usage: ./tabify [input file] -o [output file]\nThe first argument is always the input file\n");
-				printf("Arguments:\n-n: Number of spaces in 1 indent\n-o: The name of the file to output, input file will be ");
-				printf("erased if not used\n");
-				break;
+	while (--argc > 0) {
+		if ((*++argv)[0] == '-') {		
 
-			case 'n':
-				// We use *(1 + argv) rather than *++argv because c would be assigned to num rather than char
-				spacePerTab = atoi(*(1 + argv));
-				break;
+			while (c = *++argv[0]) {
+				switch (c) {
+				case 'h': case 'H': // Help case
+					printf("Tabify: Turns a file with spaces as indentation into tabs.\n");
+					printf("Usage: ./tabify [input file] -o [output file]\nThe first argument is always the input file\n");
+					printf("Arguments:\n-n: Number of spaces in 1 indent\n-o: The name of the file to output, input file will be ");
+					printf("erased if not used\n");
+					break;
 
-			case 'o':
-				strncat(outFilename, *(1 + argv), MAXFILENAME - 1);
-				break;
-			
-			default:
-				printf("Invalid input, use -h or -H for help\n");
-				break;
+				case 'n':
+					// We use *(1 + argv) rather than *++argv because c would be assigned to num rather than char
+					spacePerTab = atoi(*(1 + argv));
+					break;
+
+				case 'o':
+					strncpy(outFilename, *(1 + argv), MAXFILENAME - 1);
+					// Because strncpy won't add a \0 if full we add it ourselves
+					outFilename[MAXFILENAME - 1] = '\0';
+					break;
+
+				default:
+					printf("Invalid input, use -h or -H for help\n");
+					break;
+				}
 			}
 		}
-		
 	}
 
-	printf("%s\t%s\t%i\n", inpFilename, outFilename, spacePerTab);
+	// Attempt to open files
+	inpFile = fopen(inpFilename, "r");
+	if (!inpFile) {
+		printf("Could not open %s, does it exist?\n", inpFilename);
+		return 1;
+	}
+
+	outFile = fopen(outFilename, "w");
+	if (!outFile) {
+		printf("Could not open output file");
+		return 1;
+	}
+
+	addTabs(inpFile, outFile, spacePerTab);
+
+	fclose(inpFile);
+	fclose(outFile);
 
 	return 0;
 }
